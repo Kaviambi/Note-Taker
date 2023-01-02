58651e753cfa3bf40a27056969ab1108ce9d2649
kavic <kavicse.23@gmail.com>
2023-01-02 16:36:51 -0600

    Seperate routes are created for api and html

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const Jdata = require('./db/db.json');
-const uuid = require('uuid');
+const api = require()
 
 const app = express();
 const PORT = process.env.port || 3002;
@@ -12,18 +10,29 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+//HTML routes
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
 app.get('*', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
-app.get('/api/notes', (req, res) =>
-res.json(data));
+//API routes
+
+
+
+
+
+
+
+
+
 
 
 
