a015f6ec23f946bc63616e1e12f578015f30df86
kavic <kavicse.23@gmail.com>
2023-01-02 23:05:14 -0600

    routes added and working on notes append on front end

---
+++ routes/routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const fs = require('fs');
+const app = express();
+const path = require('path');
+
+fs.readFile("db/db.json", "utf8", (err,data) => {
+    if(err) throw err;
+
+    var notes = JSON.parse(data);
+
+    app.get("/api/notes", (req,res) => {
+        res.json(notes);
+    });
+
+    app.post("/api/notes", (req, res) => {
+        let newNote = req.body;
+        notes.push(newNote);
+        updateDb();
+        return console.log("New node added");
+    });
+
+    app.get("/api/notes/:id", (req, res) => {
+        const tipId = req.params.id;
+        res.json(notes[tipId]);
+    });
+
+    
+    app.delete("/api/notes/:id", (req, res) => {
+        notes.splice(req.params.id, 1);
+        updateDb();
+    });
+
+    app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+    app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+    app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+    function updateDb() {
+        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+            if(err) throw err;
+            return true;
+        });
+    }
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
-const api = require()
+const express = require("express");
+const fs = require("fs");
+// const path = require('path');
 
+const routes = require('./routes/routes');
 const app = express();
 const PORT = process.env.port || 3002;
 
@@ -10,32 +11,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-//HTML routes
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, './public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, './public/notes.html'))
-);
-
-app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-//API routes
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/', routes);
 
 app.listen(PORT, () =>
 console.log(`App Listening at http://localhost:${PORT}`)
