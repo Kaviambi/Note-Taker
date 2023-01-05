223a92cadd66de168b27a07c8bb59794c0713a5d
kavic <kavicse.23@gmail.com>
2023-01-05 09:12:54 -0600

    Fixing heroku error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const routes = require('./routes/routes');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true }));
 app.use(express.json());
